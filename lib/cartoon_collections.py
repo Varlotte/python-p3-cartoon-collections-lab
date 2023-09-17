7ee637482450cc73f06736862cae6059f18d4ad8
Varlotte <32116877+Varlotte@users.noreply.github.com>
2023-09-17 17:36:58 -0400

    passes 7 tests

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,23 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(names):
+    for i, name in enumerate(names):
+        print(f'{i+1}. {name}')
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(list):
+    return [f"{item.capitalize()}!" for item in list]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    for call in calls:
+        if len(call) > 4:
+            return True
+    return False         
 
-def find_the_cheese():
-    pass
+def find_the_cheese(foods):
+    for food in foods:
+        if food == "cheddar":
+            return "cheddar"
+        if food == "gouda":
+            return "gouda"
+        if food == "camembert":
+            return "camembert"
+    else:
+        return None    
